ff82959a8139ff044c4b267ba68cf125ec73f2b3
ruddro420 <aliruddro@gmail.com>
2023-05-18 21:33:05 +0600

    New Feature add - Select Button and Right Anser Color

--- src/component/Quiz/ShowQuiz/ShowQuiz.jsx
+++ src/component/Quiz/ShowQuiz/ShowQuiz.jsx
@@ -1,14 +1,69 @@
 import { useLoaderData } from "react-router-dom";
+import SingleQuiz from "../SingleQuiz/SingleQuiz";
+// eslint-disable-next-line no-unused-vars
+import { useState } from "react";
 
 const ShowQuiz = () => {
-    const albums = useLoaderData();
-    console.log(albums.data.id);
-    
+     
+    // Load Data
+    const quizLoadData = useLoaderData();
+    const quizs = quizLoadData.data.questions;
+
+     // Click Button Check
+     const [click, setClick] = useState(false);
+     const [correct,setCorrect] = useState(false);
+     const [getAnswer,setGetAnswer] = useState([]);
+     console.log(getAnswer);
+
+
+       /* console.log(quiz); */
+       const ansHandler = (option,getId,setDisabledData) => {
+
+
+        setDisabledData(true);
+        setClick(option);
+
+        const findId = quizs.find(findQuiz => findQuiz.id === getId)
+        console.log(findId.correctAnswer);
+
+        if(findId.correctAnswer === option){
+            setCorrect(true)
+            setGetAnswer(findId.correctAnswer)
+        }
+        else{
+            setCorrect(false)
+            setGetAnswer(findId.correctAnswer)
+        }
+
+        // get quiz final answer
+
+
+    }
+
+
+
+
 
     return (
-        <div>
-            Quiz Page
-        </div>
+        <>
+            <div>
+                <h2 style={{ textAlign: 'center', fontSize: '50px', marginTop: '50px', marginBottom: '50px' }}>Quiz List</h2>
+            </div>
+            <div>
+                {
+                    quizs.map(quiz => <SingleQuiz
+
+                        key={quiz.id}
+                        quiz={quiz}
+                        ansHandler={ansHandler}
+                        click={click}
+                        correct={correct}
+                        getAnswer={getAnswer}
+
+                    ></SingleQuiz>)
+                }
+            </div>
+        </>
     );
 };
 
---
+++ src/component/Quiz/SingleQuiz/SingleQuiz.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/prop-types */
+import { CardContent, Container, Grid, Typography } from "@mui/material";
+import './SingleQuiz.css';
+// eslint-disable-next-line no-unused-vars
+import { useState } from "react";
+
+const SingleQuiz = ({ quiz,ansHandler,click,correct,getAnswer}) => {
+    const [disabledData, setDisabledData] = useState(false);
+    const { id, question, options } = quiz;
+    let numberShow = 1;
+
+    console.log(click);
+
+    // For Click Button Style 
+    const getButtonStyle = (option) => {
+        return {
+            background: getAnswer === option ?  'green' : '',
+            color: getAnswer === option ? 'white' : '',
+            border: click === option ? '4px solid #F40458' : ''
+        };
+    };
+
+
+    return (
+        <div>
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 30 }} color="text.primary" gutterBottom>
+                                {question}
+                            </Typography>
+                            <Typography variant="h5" component="div">
+                                {
+
+                                    options.map(option =>
+
+                                        <button
+                                            style={getButtonStyle(option,correct)}
+                                            disabled={disabledData}
+                                            key={option}
+                                            onClick={() => ansHandler(option, id,setDisabledData)}
+                                            className={disabledData === click ? 'disabledStyle' : 'quizStyle'}
+                                        >
+                                            {numberShow++}. {option}
+                                        </button>)
+                                }
+                            </Typography>
+                        </CardContent>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
+};
+
+export default SingleQuiz;
